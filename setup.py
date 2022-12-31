5364baf0279a22596002bee1571570f25d4a2c73
John Wason <wason@wasontech.com>
2022-12-31 03:31:15 -0500

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
 from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
-    name='pyri-core',
-    version='0.1.0',
-    description='PyRI Teach Pendant Core Runtime',
-    author='John Wason',
-    author_email='wason@wasontech.com',
-    url='http://pyri.tech',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'pyri-common',
-        'RobotRaconteur',
-        'numpy',
-        'PyYAML',
-        'appdirs',
-    ],
-    tests_require=['pytest','pytest-asyncio'],
-    extras_require={
-        'test': ['pytest','pytest-asyncio']
-    },
-    entry_points = {
-        'console_scripts': ['pyri-core=pyri.core.__main__:main']
-    }
+    zip_safe=False
 )
