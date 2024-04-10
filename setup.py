427c1b6e20dd720cb2dd7c8b6aa4fa73793e8eb8
John Wason <wason@wasontech.com>
2024-04-09 23:46:37 -0400

    Bump version to 0.3.0, update deps, and remove setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages, find_namespace_packages
-
-setup(
-    package_dir={'': 'src'},
-    packages=find_namespace_packages(where='src'),
-    include_package_data=True,
-    zip_safe=False
-)
